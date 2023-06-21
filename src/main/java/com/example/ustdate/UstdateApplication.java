ff8b2c1fba51227f4f581c73025a2c82cf1b8161
AnanduBabz <38864364+AnanduBabz@users.noreply.github.com>
2023-06-21 18:07:01 +0530

    Update UstdateApplication.java

--- src/main/java/com/example/ustdate/UstdateApplication.java
+++ src/main/java/com/example/ustdate/UstdateApplication.java
@@ -2,6 +2,18 @@ package com.example.ustdate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class UstdateApplication {
@@ -9,5 +21,13 @@ public class UstdateApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UstdateApplication.class, args);
 	}
+
+	@Bean
+    public MyTelegramBot myTelegramBot() throws TelegramApiException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        MyTelegramBot bot = new MyTelegramBot();
+        botsApi.registerBot(bot);
+        return bot;
+    }
 
 }
