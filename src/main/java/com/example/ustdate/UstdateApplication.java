7fa4a5a1f51a985ab738c3c79179919b3ba23741
AnanduBabz <38864364+AnanduBabz@users.noreply.github.com>
2023-06-21 18:09:06 +0530

    Update UstdateApplication.java

--- src/main/java/com/example/ustdate/UstdateApplication.java
+++ src/main/java/com/example/ustdate/UstdateApplication.java
@@ -2,17 +2,12 @@ package com.example.ustdate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Update;
+import org.springframework.context.annotation.Bean;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import com.example.ustdate.botConfiguration.MyTelegramBot;
 
 
 @SpringBootApplication
