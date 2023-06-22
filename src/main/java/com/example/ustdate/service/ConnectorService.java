f327c4adcf0b96571a5c0768369bf22ac16e4896
AnanduBabz <38864364+AnanduBabz@users.noreply.github.com>
2023-06-22 19:02:05 +0530

    Update ConnectorService.java

--- src/main/java/com/example/ustdate/service/ConnectorService.java
+++ src/main/java/com/example/ustdate/service/ConnectorService.java
@@ -1,5 +1,7 @@
 package com.example.ustdate.service;
 
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
 public interface ConnectorService {
 	
 	public SendMessage intermediate(String userName, String chatId, String messageText);
